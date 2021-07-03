957240bc8dbf935b51222796e95dbb6a89762ab9
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-03 19:08:11 -0400

    Updates

---
+++ precache-manifest.f0188b0cf1e42a6adc034d6b5895003f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695dfccc17901b37b5b5e6f2f6d3d72b",
+    "url": "/reactTabsProject/index.html"
+  },
+  {
+    "revision": "f8259da82d7d9c2f91b7",
+    "url": "/reactTabsProject/static/css/main.77ad5ced.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/reactTabsProject/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTabsProject/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8259da82d7d9c2f91b7",
+    "url": "/reactTabsProject/static/js/main.00400493.chunk.js"
+  },
+  {
+    "revision": "4ac0495bd9b43648a9d5",
+    "url": "/reactTabsProject/static/js/runtime-main.4731ddc0.js"
+  }
+]);
